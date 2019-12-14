627c484984628f0eb02e7ace09c6ace7de8f7db2
YF <oncwnuFJYMN9mB0ZTrAMur2bBVCM@git.weixin.qq.com>
2019-12-14 18:29:46 +0800

    更新

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const ObjectId = require("mongodb").ObjectID;
 const dbUrl = "mongodb://localhost:27017/";
 app.use('/upload',express.static(path.resolve(__dirname,"upload")))
 
+//商品列表获取-全部
 app.get("/books",function(req,res){
     MongodbClient.connect(dbUrl,{ useUnifiedTopology: true },function(err,client){
         const collection=client.db("DD").collection("books");
@@ -20,6 +21,8 @@ app.get("/books",function(req,res){
         })
     })
 })
+
+//商品分类获取-全部
 app.get("/classify",function(req,res){
     MongodbClient.connect(dbUrl,{ useUnifiedTopology: true },function(err,client){
         const collection=client.db("DD").collection("classify");
@@ -27,6 +30,23 @@ app.get("/classify",function(req,res){
             result.forEach((item)=>{
                 item.image = 'http://localhost:3010/'+((item.image).replace(/\\/g,'/'))
                 console.log(item.image)
+            })
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+    })
+})
+
+//商品详情获取-一条
+app.get("/particulars",function(req,res){
+    let _id = req.query.id;
+    MongodbClient.connect(dbUrl,{ useUnifiedTopology: true },function(err,client){
+        const collection=client.db("DD").collection("books");
+        collection.find(ObjectId(_id)).toArray(function(err,result){
+            console.log(result)
+            result.forEach((item)=>{
+                item.image = 'http://localhost:3010/'+((item.image).replace(/\\/g,'/'))
             })
             res.writeHead(200,{'Content-Type':'application/json'});
             res.write(JSON.stringify(result))
